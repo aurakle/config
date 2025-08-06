ae6d9a6c36d5446524e42c08c6223d9a78e38973
Aurora Dawn <aurora.dawn@trans.fish>
2025-08-06 00:26:39 -0400

    Bruh

--- system/eos/hardware-configuration.nix
+++ system/eos/hardware-configuration.nix
@@ -10,7 +10,8 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  # breaks virtualbox?
+  # boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
