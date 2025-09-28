41eaedf6999111b2a384723bab7f245de2fae431
Aurora Dawn <aurora.dawn@trans.fish>
2025-09-28 09:14:39 -0400

    gwa

--- system/hardware/eos-intel-nvidia/configuration.nix
+++ system/hardware/eos-intel-nvidia/configuration.nix
@@ -5,7 +5,24 @@
 
   hardware.nvidia.open = false;
   services.xserver.videoDrivers = [ "nvidia" ];
-  
+
+  networking = {
+    interfaces.wlan0 = {
+      ipv4.addresses = [{
+        address = "1.1.1.80";
+        prefixLength = 24;
+      }];
+      ipv6.addresses = [];
+    };
+    defaultGateway = {
+      address = "1.1.1.1";
+      interface = "wlan0";
+    };
+    nameservers = [
+      "8.8.8.8"
+    ];
+  };
+
   # Load wifi adapter driver
   boot.extraModulePackages = with config.boot.kernelPackages; [
     rtl8812au
